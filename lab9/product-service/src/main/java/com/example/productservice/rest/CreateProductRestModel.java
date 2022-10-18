e2af412afe7212db40dc216fb41cbe3317d60e33
63070138 <73683338+63070138@users.noreply.github.com>
2022-10-18 15:55:18 +0700

    Add files via upload

---
+++ lab9/product-service/src/main/java/com/example/productservice/rest/CreateProductRestModel.java
@@ -0,0 +1,15 @@
+package com.example.productservice.rest;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class CreateProductRestModel {
+
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+
+
+}
---
+++ lab9/product-service/src/main/java/com/example/productservice/rest/ProductController.java
@@ -0,0 +1,45 @@
+package com.example.productservice.rest;
+
+import com.example.productservice.command.CreateProductCommand;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final CommandGateway commandGateway;
+
+    @Autowired
+    public ProductController(CommandGateway commandGateway) {
+        this.commandGateway = commandGateway;
+    }
+
+    @PostMapping
+    public String createProduct(@RequestBody CreateProductRestModel model){
+
+        CreateProductCommand command = CreateProductCommand.builder()
+                .productId(UUID.randomUUID().toString())
+                .title(model.getTitle())
+                .price(model.getPrice())
+                .quantity(model.getQuantity())
+                .build();
+
+        String result;
+        try {
+            result = commandGateway.sendAndWait(command);
+        } catch (Exception e){
+            result = e.getLocalizedMessage();
+        }
+
+        return result;
+    }
+
+    @DeleteMapping
+    public String  deleteProduct(){
+        return "product deleted";
+    }
+}
