aa2abb4a0bda102ad56cb66eb58c19a7958eda69
63070138 <73683338+63070138@users.noreply.github.com>
2022-10-25 16:07:09 +0700

    Add files via upload

---
+++ lab10/product-service/src/main/java/com/example/productservice/query/FindProductsQuery.java
@@ -0,0 +1,4 @@
+package com.example.productservice.query;
+
+public class FindProductsQuery {
+}
---
+++ lab10/product-service/src/main/java/com/example/productservice/query/ProductEventsHandler.java
@@ -0,0 +1,24 @@
+package com.example.productservice.query;
+
+import com.example.productservice.core.ProductEntity;
+import com.example.productservice.core.data.ProductRepository;
+import com.example.productservice.core.event.ProductCreatedEvent;
+import org.axonframework.eventhandling.EventHandler;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductEventsHandler {
+    private final ProductRepository productRepository;
+
+    public ProductEventsHandler(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    @EventHandler
+    public void on(ProductCreatedEvent event){
+        ProductEntity productEntity = new ProductEntity();
+        BeanUtils.copyProperties(event, productEntity);
+        productRepository.save(productEntity);
+    }
+}
